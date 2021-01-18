55675eb4558ecec320437bd8a718cc435c0a9c93
rayllanderson <rayllanderson@gmail.com>
2021-01-18 09:58:13 -0300

    salvando pessoa e adicionando thymeleaf à página

--- src/main/java/com/rayllanderson/controllers/PeopleController.java
+++ src/main/java/com/rayllanderson/controllers/PeopleController.java
@@ -1,15 +1,30 @@
 package com.rayllanderson.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.rayllanderson.entities.People;
+import com.rayllanderson.services.PeopleService;
 
 @Controller
 @RequestMapping("/pessoas")
 public class PeopleController {
     
-    @GetMapping("/cadastro")
-    public String register() {
+    @Autowired
+    private PeopleService service;
+    
+    @GetMapping()
+    public String start() {
+	return "register/people";
+    }
+    
+    @PostMapping()
+    public String register(People people) {
+	System.out.println(people.getName());
+	service.save(people);
 	return "register/people";
     }
 }
--- src/main/java/com/rayllanderson/services/PeopleService.java
+++ src/main/java/com/rayllanderson/services/PeopleService.java
@@ -1,5 +1,7 @@
 package com.rayllanderson.services;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +14,7 @@ public class PeopleService {
     @Autowired
     private PeopleRepository repository;
     
+    @Transactional
     public People save (People people) {
 	return repository.save(people);
     }
