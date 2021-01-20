2f589afb647405c8ce15299e5ab02158981d898f
rayllanderson <rayllanderson@gmail.com>
2021-01-20 10:39:52 -0300

    adicionando validações básicas front-end

--- src/main/java/com/rayllanderson/controllers/TelephoneController.java
+++ src/main/java/com/rayllanderson/controllers/TelephoneController.java
@@ -1,6 +1,5 @@
 package com.rayllanderson.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -8,7 +7,6 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
