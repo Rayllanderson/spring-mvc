daa274ffade04a2ee160de766184cd8513d4c21b
rayllanderson <rayllanderson@gmail.com>
2021-02-07 18:52:01 -0300

    refactor: mudando id de username (String) para id (Long)

--- src/main/java/com/rayllanderson/entities/User.java
+++ src/main/java/com/rayllanderson/entities/User.java
@@ -10,6 +10,8 @@ import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
@@ -22,6 +24,8 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import com.rayllanderson.entities.enums.RoleType;
+import com.rayllanderson.exceptions.UsernameExistsException;
+import com.rayllanderson.repositories.UserRepository;
 
 @Entity
 @Table(name = "users")
@@ -30,26 +34,37 @@ public class User implements UserDetails {
     private static final long serialVersionUID = 1L;
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
     @NotEmpty(message = "username não pode ser vazio")
     private String username;
 
     @Column(nullable = false)
     @NotEmpty(message = "senha não pode ser vazia")
     private String password;
-    
+
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
     List<People> peoples = new ArrayList<>();
-    
+
     @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable(name="user_role",
-    joinColumns = @JoinColumn(name = "user_id"),
-    inverseJoinColumns = @JoinColumn(name="role_id"))
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
 
     public User(String username) {
 	this.username = username;
     }
-    
+
+    public User(Long id) {
+	this.id = id;
+    }
+
+    public User(Long id, String username, String password) {
+	this.id = id;
+	this.username = username;
+	this.password = password;
+    }
+
     public User() {
     }
 
@@ -67,13 +82,13 @@ public class User implements UserDetails {
     public String getUsername() {
 	return username;
     }
-    
+
     public void setUsername(String username) {
-        this.username = username;
+	this.username = username;
     }
 
     public void setPassword(String password) {
-        this.password = password;
+	this.password = password;
     }
 
     @Override
@@ -103,12 +118,33 @@ public class User implements UserDetails {
     public boolean roleIsActive(String roleName) {
 	return roles.contains(new Role(RoleType.valueOf(roleName)));
     }
-    
+
+    /**
+     * Verifica se o username atual já está em uso por outro usuário
+     * 
+     * @param repository
+     * @throws UsernameExistsException There's already an User using the username
+     */
+    public void checkIfUsernameExists(UserRepository repository) throws UsernameExistsException {
+	if (repository.existsByUsername(username)) {
+	    throw new UsernameExistsException("There's already an User using the username" + username);
+	}
+    }
+
+    /**
+     * Nome não pode ser vazio, nulo nem possuir espaços em branco
+     * 
+     * @return true caso seja um nome válido, não vazio e não nulo
+     */
+    public boolean usernameIsValid() {
+	return !(username == null || this.username.isBlank() || username.isEmpty());
+    }
+
     @Override
     public int hashCode() {
 	final int prime = 31;
 	int result = 1;
-	result = prime * result + ((username == null) ? 0 : username.hashCode());
+	result = prime * result + ((id == null) ? 0 : id.hashCode());
 	return result;
     }
 
@@ -121,10 +157,10 @@ public class User implements UserDetails {
 	if (getClass() != obj.getClass())
 	    return false;
 	User other = (User) obj;
-	if (username == null) {
-	    if (other.username != null)
+	if (id == null) {
+	    if (other.id != null)
 		return false;
-	} else if (!username.equals(other.username))
+	} else if (!id.equals(other.id))
 	    return false;
 	return true;
     }
